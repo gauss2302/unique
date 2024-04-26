52ef07fd422f39a2716a651c559f7df92e513655
gauss2302 <shilov6865@gmail.com>
2024-04-26 16:56:47 +0500

    Auth_data bug issue

--- lib/features/authentication/data/datasources/auth_data.dart
+++ lib/features/authentication/data/datasources/auth_data.dart
@@ -1,3 +1,67 @@
-abstract interface class AuthData {}
+import 'package:unique_fl/features/authentication/data/models/user_model.dart';
 
-class AuthDataImpl implements AuthData {}
+abstract interface class AuthData {
+  Future<UserModel> login({
+    required String email,
+    required String password,
+  });
+
+  Future<UserModel> signup({
+    required String email,
+    required String password,
+    required String name,
+    required String photoUrl,
+  });
+  
+}
+
+class AuthDataImpl implements AuthData {
+
+
+
+  @override
+  Future<UserModel> login({
+    required String email, 
+    required String password
+    }) async {
+      try {
+        final res = await Future.delayed(
+          const Duration(seconds: 2),
+          () => UserModel.fromJson('{"id": "1", "email": "test@gmail.com"}`'),
+        );
+      } catch (e) {
+        throw Exception(e);
+      }
+
+      return UserModel.fromJson();
+    }
+
+
+
+    @override
+    Future<UserModel> signup({
+      required String email,
+      required String password,
+      required String name,
+      required String photoUrl,
+    }) async {
+      try {
+        final res = await Future.delayed(
+          const Duration(seconds: 2),
+          () => {
+            UserModel.fromMap(map: {
+              'id': '1',
+              'email': email,
+              'name': name,
+              'photoUrl': photoUrl,
+            })
+          }
+        );
+      } catch (e) {
+        throw Exception(e);
+      }
+}
+    return UserModel.fromMap();
+
+
+}
